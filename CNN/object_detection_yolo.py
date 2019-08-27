df5cc61802e6b95de0e52bacae027ff798d8da6c
naivoder <credovia@students.kennesaw.edu>
2019-08-27 09:01:20 -0400

    trained network and compressed data

--- CNN/object_detection_yolo.py
+++ CNN/object_detection_yolo.py
@@ -28,8 +28,8 @@ with open(classesFile, 'rt') as f:
 
 # Give the configuration and weight files for the model and load the network using them.
 
-modelConfiguration = "/home/naivoder/feeder/CNN/darknet-yolov3.cfg";
-modelWeights = "/home/naivoder/feeder/CNN/darknet-yolov3_final.weights";
+modelConfiguration = "/home/silversurfer/feeder/CNN/darknet-yolov3.cfg";
+modelWeights = "/home/silversurfer/feeder/CNN/weights/darknet-yolov3_final.weights";
 
 net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
